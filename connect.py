f77e00ffd3cf56ea3bf00425d20b3d931f4b44af
XinwuWang <xinwu.wang@lincolnuni.ac.nz>
2023-10-05 11:05:17 +1300

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,7 +1,7 @@
 # These details are available on the first MySQL Workbench screen
 # Usually called 'Local Instance'
-dbuser = "root" # Your MySQL username - likely 'root'
-dbpass = "XXXXX" # ---- PUT YOUR PASSWORD HERE ----
-dbhost = "localhost" 
+dbuser = "root"  # Your MySQL username - likely 'root'
+dbpass = "wxw0101--"  # ---- PUT YOUR PASSWORD HERE ----
+dbhost = "localhost"
 dbport = "3306"
 dbname = "motorkhana"
